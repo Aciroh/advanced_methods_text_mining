9a96f871d6f6df0f3a021a0a05b03c473560de13
Horatiu Bolovan <93126326+Aciroh@users.noreply.github.com>
2024-01-24 21:53:30 +0200

    Add more and more

--- Predictor.py
+++ Predictor.py
@@ -7,13 +7,13 @@ from PoS import PoS
 class Predictor:    
     @staticmethod
     def predictMostFrequent(word) -> string:
-        all_pos_path = os.path.join(os.getcwd(), 'all_pos.txt')
+        all_pos_path = os.path.join(os.getcwd(), 'Outputs/all_pos.txt')
         with open(all_pos_path) as file:
             return PoS(file.readline().split(' / ')[0])
         
     @staticmethod
     def predictGlobalProbability(word) -> string:
-        words_grouped = os.path.join(os.getcwd(), 'words_grouped.txt')
+        words_grouped = os.path.join(os.getcwd(), 'Outputs/words_grouped.txt')
         pos_list=[]
         with open(words_grouped) as file:
             curr_word=None
